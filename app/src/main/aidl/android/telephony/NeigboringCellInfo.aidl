a8991903240eba61634d71e5997898bc41581a6f
Vapor1997 <893201500@qq.com>
2017-11-25 23:55:55 +0800

    //m6

---
+++ app/src/main/aidl/android/telephony/NeigboringCellInfo.aidl
@@ -0,0 +1,9 @@
+
+package android.telephony;
+
+
+interface NeigboringCellInfo {
+
+    void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
+            double aDouble, String aString);
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.mobileguard.m6cleancache.entity;
+
+import android.graphics.drawable.Drawable;
+
+public class CacheInfo {
+    public String packagename;
+    public long cacheSize;
+    public Drawable appIcon;
+    public String appName;
+}
